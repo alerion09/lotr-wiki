399ad15eb7d24f3e1e7243bfd03bf634499d5f38
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-04-02 16:31:53 +0200

    Created components Books, Movies, Characters, HomePage. / Created Routers links.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import {lotrApiKey} from './config'
-import Navigation from './Navigation'
+import React from 'react';
+import {lotrApiKey} from './config';
+
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Books from './Books';
+import Movies from './Movies';
+import Characters from './Characters';
+import HomePage from './HomePage';
+import Navigation from './Navigation';
+
+
 function App() {
   return (
-    <>
+    <Router>
       <Navigation />
-    </>
+      <Switch>
+          <Route exact path='/'>
+              <HomePage />
+          </Route>
+          <Route path='/books'>
+              <Books />
+          </Route>
+          <Route path='/movies'>
+              <Movies />
+          </Route>
+          <Route path='/characters'>
+              <Characters />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,15 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import {Link} from 'react-router-dom';
 const Navigation = () => {
     return (
-        <nav> 
-            <ul>
-                <li>Books</li>
-                <li>Movies</li>
-                <li>Characters</li>
-            </ul>
-        </nav>
-    )
+            <nav> 
+                <ul>
+                    <li><Link to='/'><h1>The Lord of The Rings Wiki</h1></Link></li>
+                </ul>
+                <ul>
+                    <li><Link to='/books'>Books</Link></li>
+                    <li><Link to='/movies'>Movies</Link></li>
+                    <li><Link to='/characters'>Characters</Link></li>
+                </ul>
+            </nav>
+    );
 }
 
-export default Navigation
+export default Navigation;
