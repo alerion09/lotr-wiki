ad2661198bba1f2237ac2236fc19ddb60dd6e796
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-04-02 16:51:00 +0200

    Added CSS class .link

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,12 +4,12 @@ const Navigation = () => {
     return (
             <nav> 
                 <ul>
-                    <li><Link to='/'><h1>The Lord of The Rings Wiki</h1></Link></li>
+                    <li><Link className='link' to='/'><h1>The Lord of The Rings Wiki</h1></Link></li>
                 </ul>
                 <ul>
-                    <li><Link to='/books'>Books</Link></li>
-                    <li><Link to='/movies'>Movies</Link></li>
-                    <li><Link to='/characters'>Characters</Link></li>
+                    <li><Link className='link' to='/books'>Books</Link></li>
+                    <li><Link className='link' to='/movies'>Movies</Link></li>
+                    <li><Link className='link' to='/characters'>Characters</Link></li>
                 </ul>
             </nav>
     );
